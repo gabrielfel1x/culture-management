6e26b3a3e4464045549df326f2ff7dcfbcdaab2d
gabrielfel1x <gabrielfelxx@gmail.com>
2024-08-14 14:03:53 -0300

    rm unecessary coments

--- culture_management/culture_management/urls.py
+++ culture_management/culture_management/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signin/', views.signin, name='signin'),
+    path('', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(next_page='signin'), name='logout'),
     path('', include('app_culture_management.urls')),
